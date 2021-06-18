830312209b24315903f06c234f2ea7c0031f1254
Aimeos <aimeos@aimeos.org>
2021-06-18 09:19:57 +0200

    Minor coding style improvement

--- manifest.php
+++ manifest.php
@@ -1,11 +1,11 @@
 <?php
 
-return array(
+return [
 	'name' => 'ai-filesystem',
-	'depends' => array(
+	'depends' => [
 		'aimeos-core',
-	),
-	'include' => array(
+	],
+	'include' => [
 		'lib/custom/src',
-	),
-);
+	],
+];
